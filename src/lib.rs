92e4642c1fc8dc661411643ba6e5a2e05f311166
liming <421100037@qq.com>
2015-12-13 22:11:15 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+extern crate rustc_serialize;
+use self::rustc_serialize::json::Json;
+use self::rustc_serialize::json::ToJson;
+
+use std::str::FromStr;
+
+#[macro_export]
+macro_rules! json {
+    ($e:expr) => (Json::from_str($e).unwrap());
+}
+
+#[macro_export]
+macro_rules! json_path {
+    ($o:expr;$($y:expr),*) => {{
+        let mut data = $o;
+        $(
+            if data.is_array() {
+                let array = data.as_array().unwrap();
+                let index = usize::from_str($y).unwrap();
+                data = array.get(index).unwrap();
+            } else {
+                data = data.find($y).unwrap();
+            }
+        )*
+        data
+    }}
+}
+
+#[macro_export]
+macro_rules! json_str {
+    ($o:expr;$($y:expr),*) => {{
+        let data = json_path!($o;$($y),*);    
+        data.as_string().unwrap()
+    }}
+}
+
+#[macro_export]
+macro_rules! json_i64 {
+    ($o:expr;$($y:expr),*) => {{
+        let data = json_path!($o;$($y),*);    
+        if data.is_string() {
+            let my_str = data.as_string().unwrap();
+            i64::from_str(my_str).unwrap()
+        } else {
+            data.as_i64().unwrap()
+        }
+    }}
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+#[macro_use]
+extern crate easy_util;
+
+extern crate rustc_serialize;
+use rustc_serialize::json::Json;
+use rustc_serialize::json::ToJson;
+
+use std::str::FromStr;
+
+fn main() {
+    let my_str = format!(r#"
+        {{
+            "a": [
+            {{
+                "index":0,
+                "content":"nothing"
+            }},
+            {{
+                "index":1,
+                "content":"something"
+            }},
+            {{
+                "index":"2"
+            }}
+            ]
+        }}
+    "#);
+    let my_json = json!(&my_str);
+    let after_json = json_i64!(&my_json; "a", "2", "index");
+    println!("{}", after_json);
+    let content = json_str!(&my_json; "a", "1", "content");
+    println!("{}", content);
+}
